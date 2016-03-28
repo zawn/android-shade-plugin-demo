4e70e8c8c025c9561aa3617986ce0ad4588ea0a5
zhangzhenli <zhangzhenli@live.com>
2016-03-28 11:17:35 +0800

    提交测试代码

---
+++ myapplication2/src/main/java/com/house365/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2015-2016, House365. All rights reserved.
+ */
+
+package com.house365.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
